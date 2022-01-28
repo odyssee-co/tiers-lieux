426e692c8e1c7caf578f52f78e861a9b029ab0ab
Matthieu MASTIO <matthieu@smlv800172.ifsttar.fr>
2022-01-28 18:17:53 +0100

    nb_offices

--- code/nb_offices.py
+++ code/nb_offices.py
@@ -1,4 +1,5 @@
 import router
+import time
 import argparse
 import optimizer
 from matplotlib import pyplot
@@ -13,17 +14,26 @@ if __name__ == "__main__":
     output_path = args.out
     r = router.Router(data_path)
     saved_df = r.get_saved_distance()
-
-    out = []
-    for i in range(1, int(saved_df.shape[1]/2)):
-        res = optimizer.evolutionary(saved_df, i+1, nb_it=200)
-        average = 2*res[0]/(1000*saved_df.shape[0])
-        out.append(average)
+    #saved_df = saved_df.sample(round(saved_df.shape[0]*0.01))
 
     with open(output_path, "w") as f:
-        for e in out:
-            f.write(str(e)+"\n")
+        f.write("i;time_mip;res_mip;time_rw;res_rw;time_evo;res_evo\n")
+        for i in range(2, int(saved_df.shape[1]/2)):
+            print(i, flush=True)
+            begin = time.time()
+            res_mip = optimizer.mip(saved_df, i, solver="cbc")
+            time_mip = time.time() - begin
+            begin = time.time()
+            res_rw = optimizer.random_weighted(saved_df, i)
+            time_rw = time.time() - begin
+            begin = time.time()
+            res_evo = optimizer.evolutionary(saved_df, i)
+            time_evo = time.time() - begin
+            f.write(f"{i};{time_mip};{res_mip};{time_rw};{res_rw};{time_evo};{res_evo}\n")
+            f.flush()
+    """
     pyplot.plot(range(2, len(out)+2),out)
     pyplot.xlabel('nb_tierslieux')
     pyplot.ylabel('saved_distance_per_employee_per_day (km)')
     pyplot.show() 
+    """
